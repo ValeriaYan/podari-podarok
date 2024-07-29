36ca846cd0040583677164b5d00ec9d137d265f8
Valeria <valeria.yanycheva@gmail.com>
2024-07-29 21:15:01 +0600

    implement tabs on mobile

--- scripts/tabs.js
+++ scripts/tabs.js
@@ -1,11 +1,7 @@
 const tabsContainers = document.querySelectorAll('.Tabs_tabRoot');
+const mobileMenus = document.querySelectorAll('.react-select-container-red');
 
-function showContent(currentTabName, tabName) {
-    const currentContents = document.querySelectorAll(`[data-tab="${currentTabName}"]`);
-    currentContents.forEach((currentContent) => {
-        currentContent.classList.remove('_active');
-        console.log(currentContent)
-    })
+function showContent(tabName) {
     const contents = document.querySelectorAll(`[data-tab="${tabName}"]`);
     contents.forEach((content) => {
         content.classList.add('_active');
@@ -15,15 +11,54 @@ function showContent(currentTabName, tabName) {
     })
 }
 
+function removeContent(currentTabName) {
+    const currentContents = document.querySelectorAll(`[data-tab="${currentTabName}"]`);
+    currentContents.forEach((currentContent) => {
+        currentContent.classList.remove('_active');
+    })
+}
+
+function setPlaceholder(container, tab) {
+    const placeholder = container.querySelector('.Tabs_placeholder');
+    if(placeholder) {
+        placeholder.textContent = tab.textContent;
+    }
+}
+
+function setActiveTab(tab) {
+    tab.classList.add('Tabs_selected');
+    const similarTabs = document.querySelectorAll(`[data-tab-btn="${tab.dataset.tabBtn}"]`);
+    similarTabs.forEach((tab) => {
+        tab.classList.add('Tabs_selected');
+    })
+}
+
+function removeActiveTab(currentTab) {
+    currentTab.classList.remove('Tabs_selected');
+    const similarTabs = document.querySelectorAll(`[data-tab-btn="${currentTab.dataset.tabBtn}"]`);
+    similarTabs.forEach((tab) => {
+        tab.classList.remove('Tabs_selected');
+    })
+}
+
 tabsContainers.forEach((container) => {
-    let currentTab = container.querySelector('.Tabs_selected');
     const tabs = container.querySelectorAll('.Tabs_tab');
     tabs.forEach((tab) => {
         tab.addEventListener('click', () => {
-            currentTab.classList.remove('Tabs_selected');
-            tab.classList.add('Tabs_selected');
-            showContent(currentTab.dataset.tabBtn, tab.dataset.tabBtn);
-            currentTab = tab;
+            let currentTab = container.querySelector('.Tabs_selected');
+            removeActiveTab(currentTab);
+            setActiveTab(tab);
+            setPlaceholder(container, tab);
+            removeContent(currentTab.dataset.tabBtn)
+            showContent(tab.dataset.tabBtn);
         })
+    })
+})
+
+mobileMenus.forEach((menu) => {
+    const list = menu.querySelector('.react-select-red__menu');
+    menu.addEventListener('click', () => {
+        console.log(menu)
+        list.classList.toggle('_active');
     })
 })
