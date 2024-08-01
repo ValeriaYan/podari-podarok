31b2d0e543295f5e63a00c0fef396ff450ccc035
Valeria <valeria.yanycheva@gmail.com>
2024-08-01 17:16:15 +0600

    implement plus, minus, trash btn in cart

---
+++ scripts/cart-need.js
@@ -0,0 +1,25 @@
+const needs = document.querySelectorAll('.CartNeeds_need');
+
+needs.forEach((need) => {
+    const plusBtn = need.querySelector('.Counter_plus');
+    const minusBtn = need.querySelector('.Counter_minus');
+    const input = need.querySelector('.Counter_input');
+    const trash = need.querySelector('.CartNeeds_trash');
+
+    plusBtn.addEventListener('click', () => {
+        input.value = +input.value + 1;
+    })
+
+    minusBtn.addEventListener('click', () => {
+        if(input.value > 1) {
+            input.value = +input.value - 1;
+        }
+    })
+
+    trash.addEventListener('click', () => {
+        if(!need.nextElementSibling.classList.contains('CartNeeds_need') && !need.previousElementSibling.classList.contains('CartNeeds_need')) {
+            need.parentElement.remove();
+        }
+        need.remove();
+    })
+})
