d58b30d6bdd83241165ebf23eb2e887d5dfe232c
agletd <78316149+agletd@users.noreply.github.com>
2024-08-13 18:58:11 +0400

    разные правки и новые селекты

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -396,43 +396,43 @@ popups?.forEach((popup) => {
 
 })
 
-// Select
+// Select old
 
-const selectContainers = document.querySelectorAll('.react-select-container');
+// const selectContainers = document.querySelectorAll('.react-select-container');
 
-function setActiveOption(options, option, placeholder) {
-    option.classList.add('react-select__option--is-selected');
-    placeholder.textContent = option.textContent;
-    options.forEach((o) => {
-        if(o !== option) {
-            o.classList.remove('react-select__option--is-selected');
-        }
-    })
-}
+// function setActiveOption(options, option, placeholder) {
+//     option.classList.add('react-select__option--is-selected');
+//     placeholder.textContent = option.textContent;
+//     options.forEach((o) => {
+//         if(o !== option) {
+//             o.classList.remove('react-select__option--is-selected');
+//         }
+//     })
+// }
 
-selectContainers?.forEach((select) => {
-    const options = select.querySelectorAll('.react-select__option');
-    const menuList = select.querySelector('.react-select__menu-list');
-    const placeholder = select.querySelector('.react-select__placeholder');
+// selectContainers?.forEach((select) => {
+//     const options = select.querySelectorAll('.react-select__option');
+//     const menuList = select.querySelector('.react-select__menu-list');
+//     const placeholder = select.querySelector('.react-select__placeholder');
 
-    select.addEventListener('click', () => {
-        menuList?.classList.toggle('_active');
-    })
-    menuList?.addEventListener('click', (e) => {
-        const option = e.target;
-        if(option.classList.contains('react-select__option')) {
-            setActiveOption(options, option, placeholder);
-        }
-    })
+//     select.addEventListener('click', () => {
+//         menuList?.classList.toggle('_active');
+//     })
+//     menuList?.addEventListener('click', (e) => {
+//         const option = e.target;
+//         if(option.classList.contains('react-select__option')) {
+//             setActiveOption(options, option, placeholder);
+//         }
+//     })
 
-    document.addEventListener('click', outsideEvtListener);
+//     document.addEventListener('click', outsideEvtListener);
     
-    function outsideEvtListener(e) {
-        if (!e.target.closest('.react-select-container')) {
-            menuList?.classList.remove('_active')
-        }
-    }
-})
+//     function outsideEvtListener(e) {
+//         if (!e.target.closest('.react-select-container')) {
+//             menuList?.classList.remove('_active')
+//         }
+//     }
+// })
 
 
 // Slider
@@ -507,7 +507,14 @@ multiSliders?.forEach((item, key) => {
                   slidesToShow: 2,
                   slidesToScroll: 1,
                 }
-              }
+              },
+              {
+                  breakpoint: 476,
+                  settings: {
+                    slidesToShow: 1,
+                    dots: true,
+                  }
+                }
           ]
     })
 })
@@ -719,4 +726,26 @@ sections?.forEach((section) => {
 
 })
 
-const players = Plyr.setup('.js-player');
+$(".news_category").each(function(index, value) {
+            
+    console.log($(value).prop( "checked"))
+});
+
+
+//select
+$(document).ready(function() {
+    $('.custom').select2({ width: '100%',minimumResultsForSearch: -1 });
+
+
+    if( $('[data-fancybox]').length )     {
+        Fancybox.bind("[data-fancybox]", {});
+    }
+
+    if( $('.js-player').length )     {
+        const players = Plyr.setup('.js-player');
+    }
+
+
+
+});
+
